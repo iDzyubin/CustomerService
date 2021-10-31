0ebb15da79fa2ca13f0bd61aa5490ab4ffac29eb
idzyubin <idzyubin@yahoo.com>
2021-10-31 19:32:40 +0300

    Добавил сущность покупателя и интерфейс для работы с ним

---
+++ CustomerService.Contract/Entities/Customer.cs
@@ -0,0 +1,28 @@
+﻿namespace CustomerService.Contract.Entities
+{
+    /// <summary>
+    ///     Сущность покупателя
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        ///     Идентификатор покупателя
+        /// </summary>
+        public long Id { get; set; }
+        
+        /// <summary>
+        ///     Имя
+        /// </summary>
+        public long FirstName { get; set; }
+        
+        /// <summary>
+        ///     Фамилия
+        /// </summary>
+        public long LastName { get; set; }
+        
+        /// <summary>
+        ///     Отчество
+        /// </summary>
+        public long MiddleName { get; set; }
+    }
+}
---
+++ CustomerService.Contract/Interfaces/ICustomerService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using CustomerService.Contract.Entities;
+
+namespace CustomerService.Contract.Interfaces
+{
+    /// <summary>
+    ///     Интерфейс для взаимодействия с сервисом покупателя
+    /// </summary>
+    public interface ICustomerService
+    {
+        /// <summary>
+        ///     Добавить нового покупателя в систему
+        /// </summary>
+        /// <param name="customer">Данные о добавляемом покупателе</param>
+        /// <returns></returns>
+        Customer AddCustomer(Customer customer);
+
+        /// <summary>
+        ///     Обновить информацию об уже имеющемся покупателе
+        /// </summary>
+        /// <param name="customerId">Идентификатор покупателя, которого обновляем</param>
+        /// <param name="customer">Обновленная информация</param>
+        /// <returns>Обновленная информация о покупателе</returns>
+        Customer UpdateCustomer(long customerId, Customer customer);
+
+        /// <summary>
+        ///     Удалить покупателя из системы
+        /// </summary>
+        /// <param name="customerId">Идентификатор покупателя, которого удаляем</param>
+        /// <returns>Информация об удаленном покупателе</returns>
+        Customer DeleteCustomer(long customerId);
+
+        /// <summary>
+        ///     Получить покупателя по идентификатору
+        /// </summary>
+        /// <param name="customerId">Идентификатор покупателя</param>
+        /// <returns>Искомый покупатель</returns>
+        Customer GetCustomerById(long customerId);
+
+        /// <summary>
+        ///     Вернуть всех покупателей
+        /// </summary>
+        /// <returns>Коллекция покупателей</returns>
+        List<Customer> GetAllCustomers();
+    }
+}
